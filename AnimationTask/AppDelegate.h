609de84bc5f2b091d15baa2b1e7610d2bcdf6b1c
zangqilong <zangqilong@gmail.com>
2016-04-06 13:07:26 +0800

    Initial Commit

---
+++ AnimationTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationTask
+//
+//  Created by 臧其龙 on 16/4/6.
+//  Copyright © 2016年 臧其龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationTask
+//
+//  Created by 臧其龙 on 16/4/6.
+//  Copyright © 2016年 臧其龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
