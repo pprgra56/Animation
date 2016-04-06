cc659deda7b0aaa3c2cecd1a9abd3c5af40721d9
zangqilong <zangqilong@gmail.com>
2016-04-06 13:21:42 +0800

    initial commit

--- AnimationTask/ViewController.m
+++ AnimationTask/ViewController.m
@@ -8,20 +8,47 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString * const kCellID = @"kCellID";
+
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) NSArray<NSString *> *animationNames;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.animationNames = @[@"ViewAnimation", @"LayerAnimation", @"TransitionAnimation", @"DynamicAnimation", @"ParticleAnimation"];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - TableView DataSource && Delegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.animationNames? self.animationNames.count:0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellID];
+    cell.textLabel.text = self.animationNames[indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+   // TODO: Finish Click Action
+    
 }
 
 @end
